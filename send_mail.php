bfa1e6ee9034f6f4ed49b265991ba84d5e4edf4c
srinath reddy <domains@srinathreddy.tech>
2018-03-21 20:48:29 +0530

    blal bla

--- send_mail.php
+++ send_mail.php
@@ -1,15 +1,23 @@
 <?php
-	if (isset($_POST["submit"])) {
+header("access-control-allow-origin: *");
+	if (isset($_POST["name"])) {
 		$name = $_POST['name'];
 		$email = $_POST['email'];
         $mobile = $_POST['mobile'];
 		$message = $_POST['message'];
+        
+        echo $name;
+        echo "<br>";
+        echo $email;echo "<br>";
+        echo $mobile;echo "<br>";
+        echo $message;echo "<br><hr>";
+        
 		
-		$from = $email; 
+		$from = "From: {$email}\r\n";
 		$to = 'info@srinathreddy.tech'; 
 		$subject = 'Message from Enquiry form';
 		
-		$body ="From: $name\n E-Mail: $email\n Mobile: $mobile Message:\n $message";
+		$body ="From: $from <br> E-Mail: $email <br> Mobile: $mobile <br> Message: $message <hr>";
 		// Check if name has been entered
 		//if (!$_POST['name']) {
 		//	$errName = 'Please enter your name';
@@ -29,12 +37,19 @@
 		//	$errHuman = 'Your anti-spam is incorrect';
 		//}
 // If there are no errors, send the email
+        
+        echo $from;echo "<br>";
+        echo $to;echo "<br>";
+        echo $subject;echo "<hr>";
+        echo $body;echo "<br>";
 
 	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+		echo $result='<div class="alert alert-success">Thank You! I will be in touch</div>';
 	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
+		echo $result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later.</div>';
 	}
 
 	}
-?>
+else
+    echo "invalid request";
+?>    
